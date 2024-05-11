ccdb89e5722f30eb712368e1e7469f54d044ede4
Ayaz <ayazag960@gmail.com>
2024-05-11 13:19:08 +0500

    changes-into-file:formating doucment for better under standing

--- src/ProductDeatil.jsx
+++ src/ProductDeatil.jsx
@@ -1,35 +1,44 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import { useLocation } from "react-router-dom";
 import { useParams } from "react-router-dom";
-function ProductDeatil(){
+function ProductDeatil() {
   const { productID } = useParams();
-    const [oneproduct1, setoneproduct1] = useState([]);
-    const location = useLocation();
+  const [oneproduct1, setoneproduct1] = useState([]);
+  const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const paramvalue = queryParams.get('productID');
-    
-    useEffect(() => {
-        fetch(`https://fakestoreapi.com/products/${paramvalue}`)
-          .then((res) => res.json())
-          .then((data) => {
-            setoneproduct1(data);
-          });
-      }, []);
-    return(
+  const paramvalue = queryParams.get("productID");
+
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${paramvalue}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setoneproduct1(data);
+      });
+  }, []);
+  return (
     <div>
-      <Header/>
-    <div className="loop-map space-y-6 bg-sky-200">
-     <div className="font-bold justify-center text-3xl font-serif flex text-slate-950">{oneproduct1.title}</div>
-    <div className="flex justify-center"> 
-    <img className="h-[300px] w-auto border border-black hover:animate-pulse shadow-2xl shadow-slate-950" src={oneproduct1.image}/>
+      <Header />
+      <div className="loop-map space-y-6 bg-sky-200">
+        <div className="font-bold justify-center text-3xl font-serif flex text-slate-950">
+          {oneproduct1.title}
+        </div>
+        <div className="flex justify-center">
+          <img
+            className="h-[300px] w-auto border border-black hover:animate-pulse shadow-2xl shadow-slate-950"
+            src={oneproduct1.image}
+          />
+        </div>
+
+        <div className=" flex flex-wrap justify-center m-3 font-serif">
+          <p className="font-bold text-2xl  text-slate-950">Description:</p>{" "}
+          {oneproduct1.description}
+        </div>
+        <div className="font-bold text-3xl text-slate-950 flex justify-center font-serif">
+          Price: ${oneproduct1.price}
+        </div>
+      </div>
     </div>
-     
-        {/* <img src={oneproduct1.thumbnail}  />  */}
-        <div className=" flex flex-wrap justify-center m-3 font-serif"><p className="font-bold text-2xl  text-slate-950">Description:</p> {oneproduct1.description}</div>
-        <div className="font-bold text-3xl text-slate-950 flex justify-center font-serif">Price: ${oneproduct1.price}</div>
-        </div>
-        </div>
-    )
+  );
 }
 export default ProductDeatil;
--- src/ShowData.jsx
+++ src/ShowData.jsx
@@ -1,29 +1,30 @@
-import { Link } from 'react-router-dom';
-import React from 'react'
-function ShowData({title,price,img ,rating,id}) {
-  
+import { Link } from "react-router-dom";
+import React from "react";
+function ShowData({ title, price, img, rating, id }) {
   return (
-<div className='flex  '>
-<Link to={`/ProductDeatil?productID=${id}`}><div className='font-semibold w-[500px] h-[300px] gap gap-5 md:opacity-75 cursor-pointer hover:opacity-100 shadow-xl shadow-slate-600 pl-[120px] pt-10' >
-  
+    <div className="flex  ">
+      <Link to={`/ProductDeatil?productID=${id}`}>
+        <div className="font-semibold w-[500px] h-[300px] gap gap-5 md:opacity-75 cursor-pointer hover:opacity-100 shadow-xl shadow-slate-600 pl-[120px] pt-10">
+          <img className="h-24 w-24 " src={img} />
+          <h1>{title}</h1>
+          <br></br>
+          <p className="">${price}</p>
+          <div className="space-x-1">
+            <button className="bg-black text-white p-2" type="click">
+              Add to Cart
+            </button>
+            <button className="bg-black text-white p-2" type="click">
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </Link>
 
-   <img className='h-24 w-24 ' src={img} />
-   <h1 >{title}</h1>
-   <br></br>
-   <p className=''>${price}</p>
-   <div className='space-x-1'>
-   <button className='bg-black text-white p-2' type='click'>Add to Cart</button>
-   <button className='bg-black text-white p-2' type='click'>Buy Now</button>
-   </div>
-</div>
-</Link>
-
-<div className='font-bold flex flex-wrap'>
-  <span>{rating}</span>
-</div>
-
-</div>
-  )
+      <div className="font-bold flex flex-wrap">
+        <span>{rating}</span>
+      </div>
+    </div>
+  );
 }
 
-export default ShowData
+export default ShowData;
